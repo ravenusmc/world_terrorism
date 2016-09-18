d87a73841d0a32be1ea303e9aa3f9cec917ba877
ravenusmc <mcuddy77@gmail.com>
2016-09-18 17:03:01 -0400

    added comments and a main function

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+#importing all libraries which will be used. 
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt
 
-ufo = pd.read_csv('attacks_data.csv')
-print(ufo.head())
+#This is the main function which will start the program. 
+def main():
+  print("\033c")
+  print("------------------------")
+  print("-------Welcome to-------")
+  print("--World Terrorism Data--")
+  print("------------------------")
+  input("Press enter to continue to main menu")
+  mainMenu()
+
+#This function provides the main menu to the user. From here, they will be able to select what they want to do. 
+def mainMenu():
+  print("\033c")
+
+main()
